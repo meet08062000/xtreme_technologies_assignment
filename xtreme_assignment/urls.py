e29bc8da7bf45aee7480224e90155bb7662530e9
Meet Shah <meet8june@gmail.com>
2022-01-05 13:46:28 +0530

    resolving errors

--- xtreme_assignment/urls.py
+++ xtreme_assignment/urls.py
@@ -23,4 +23,8 @@ from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url(r'^media/(?P<path>.*)$', serve,
+        {'document_root':       settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$', serve,
+        {'document_root': settings.STATIC_URL}),
 ]
