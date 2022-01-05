ced37d63217191acbab038091c9691053bc7424b
Meet Shah <meet8june@gmail.com>
2022-01-05 14:10:13 +0530

    resolving errors

--- xtreme_assignment/settings.py
+++ xtreme_assignment/settings.py
@@ -145,3 +145,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
